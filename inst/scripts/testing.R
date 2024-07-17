4b4d50d91c446f911178a2038238e5969013679d
Chao-Jen Wong <chao-jen.wong@seattlechildrens.org>
2024-07-17 08:48:57 -0700

    edit NAMESAPCE and testing script

--- inst/scripts/testing.R
+++ inst/scripts/testing.R
@@ -22,10 +22,27 @@ file.exists(path)
 #
 # 2. testing get_fastq_path()
 #
-df <- get_fastq_path(path, pattern='\\.fq.gz$',
-                     reads_pattern='_1|_2',
-                     read1_pattern='_1')
 
+# if novogene
+df_GBe6 <- get_fastq_path(path, pattern='\\.fq.gz$',
+                          reads_pattern='_1|_2',
+                          read1_pattern='_1')
+# Gabe's sample_id is self-explanatory such that we don't need a sample
+# sheet to make the nextflow sample sheet
+df_GBe6 <- df_GBe6 %>%
+  dplyr::mutate(sample_id =
+                  str_replace(fq_sample_name, '_CKDL.*', ''),
+                cell_line =
+                  str_split(fq_sample_name, '_', simplify=TRUE)[, 2],
+                antibody  =
+                  str_split(fq_sample_name, '_', simplify=TRUE)[, 4],
+                .before='fq_sample_name') %>%
+  dplyr::mutate(sample = cell_line,
+                target_or_control = 'target',
+                single_end = FALSE,
+                .before = 'fq_sample_name')
+
+# if regular: this df will need a sample sheet to make the NF sample sheet
 path <-  '/archive/sarthy_j/FASTQs/240708_VH01189_307_AAFYY2YM5/Unaligned/Project_jsarthy'
 df <- get_fastq_path(path, pattern='\\.fastq.gz$',
                      reads_pattern = '_R1|_R2',
@@ -58,4 +75,5 @@ tidy_sample_sheet(master_sheet,
                   treatment = NULL,
                   sample = NULL)
 
+# GBe6
 
