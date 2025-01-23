9d0e565f6f5acc3406f026cb163d346cbca8c5a1
chaochaowong <chao-jen.wong@seattlechildrens.org>
2025-01-23 13:01:49 -0800

    improve code quality

--- R/sanitize_nf_sheet.R
+++ R/sanitize_nf_sheet.R
@@ -1,13 +1,19 @@
-sanitize_nf_sheet <- function(x, style = c('scri', 'nf-core' )) {
-  # x: df or a sheet
+sanitize_nf_sheet <- function(x, remove_na_cols = TRUE,
+                              style = c('scri', 'nf-core' )) {
+  # x: data.frame
   
-  # check if there is special 
-  x  <- x %>%
-    dplyr::mutate(sample_id = case_when(
-      str_detect(sample_id, '\\(|\\)') ~ str_replace_all(sample_id, '\\(|\\)', ''),
-      str_detect(sample_id, ';') ~ str_replace(sample_id, ';', ''),
-      .default = sample_id)
-    )
-
+  # 1. replace special character
+  x <- x %>%
+    dplyr::mutate(sample_id = str_replace_all(sample_id, "\\(", "_")) %>%  # Replace '(' with '_'
+    dplyr::mutate(sample_id = str_replace_all(sample_id, "[^a-zA-Z0-9_-]", ""))  # Remove all other special characters
+  
+  # 2. delete columns without entries (NA)
+  if (remove_na_cols) {
+    
+  }
 
 }
+
+x <- data.frame(sample_id = 'apple;11(orange)-kiwi_grapes',
+                empty = NA)
+y <- sanitize_nf_sheet(x)
