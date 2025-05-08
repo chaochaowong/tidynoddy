b745330c36b906eb96b174130cc4c7de492b259e
chaochaowong <chao-jen.wong@seattlechildrens.org>
2025-05-08 21:01:53 +0000

    update sample sheetvalidation for nf-core/cutandrun pipeline

--- R/validate_nf_sheet.R
+++ R/validate_nf_sheet.R
@@ -5,16 +5,29 @@
 #' read1, and read2. In addition, validate the "sample" group to include at
 #' least one target sample
 #'
-#' @param nf_sample_sheet A path to the nextflow cutandrun pipeline sample sheet
-#'
+#' @param nf_sample_sheet A path to the nextflow cutandrun pipeline sample sheet.
+#' @param nf_pipeline character string, 'scri' or 'nf-core', to specify which pipeline you are running.
 #' @example
+#' 
+#' \dontrun{
+#' validate('nf-core-sample-sheet.csv', 'nf-core')
+#' }
+#' 
 #' @importFrom readr read_csv
+#' 
+#' @author Chao-Jen
 #' @export
 #'
 
 
-validate_nf_sheet <- function(nf_sample_sheet) {
+validate_nf_sheet <- function(nf_sample_sheet, 
+                              nf_pipeline = c('scri', 'nf-core')) {
 
+  nf_platform <- match.arg(nf_platform)
+  # now nf_platform is guaranteed to be either "scri" or "nf-core"
+  message("Running on pipeline using: ", nf_platform)
+  
+  # sanity tools
   is_vector_unique <- function(vec) {
     length(vec) == length(unique(vec))
   }
@@ -35,9 +48,14 @@ validate_nf_sheet <- function(nf_sample_sheet) {
   tb <- readr::read_csv(nf_sample_sheet)
 
   # 2. check mandatory columns
-  mandatory_columns <- c('sample_id', 'sample', 'single_end',
-                         'target_or_control',
-                         'read1', 'read2')
+  if (nf_pipeline == 'scri')
+    mandatory_columns <- c('sample_id', 'sample', 'single_end',
+                           'target_or_control',
+                           'read1', 'read2')
+  if (nf_pipeline == 'nf-core')
+    mandatory_columns <- c('group', 'replicate',
+                           'fastq_1', 'fastq_2',
+                           'control')
 
   tb <- read_csv(nf_sample_sheet)
   assert <- (mandatory_columns %in% names(nf_sample_sheet))
